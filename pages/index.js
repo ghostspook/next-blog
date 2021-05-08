456678c74e3bb310ddfbc04a9198b3cb5a853268
Rafael Castillo <pcastill@mac.com>
2021-05-07 22:40:36 -0500

    Added layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+import Layout from '../components/layout'
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Home() {
     return (
-      <>
+        <Layout>
+        <Head>
+          <title>Next.js starter site</title>
+        </Head>
         <h1>Next.js starter site</h1>
         <p>This is a demonstration blog using <a href="https://nextjs.org/">Next.js</a>.</p>
-        <p>Find out more about us <Link href="/about">here</Link></p>
-      </>
+        <p>Find out more <Link href="/about"><a>about us...</a></Link></p>
+      </Layout>
     );
   }
